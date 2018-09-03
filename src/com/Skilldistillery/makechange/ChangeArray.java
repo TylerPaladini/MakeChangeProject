a3118e250073eaef67bf90bf4150c3517c933d56
Tyler Paladini <TylerPaladini@yahoo.com>
2018-09-02 22:59:23 -0600

    Condensed Code

---
+++ src/com/Skilldistillery/makechange/ChangeArray.java
@@ -0,0 +1,25 @@
+package com.Skilldistillery.makechange;
+
+public class ChangeArray {
+	double money[] = { 20, 10, 5, 1, .25, .10, .049, .009 };
+	String denominations[] = { "twenties: ", "tens: ", "fives: ", "ones: ", "quarters: ", "dimes: ", "nickels: ",
+			"pennies: " };
+
+	public void cashArry(double total) {
+		double changeDue = total;
+		
+		for (int i = 0; i < money.length; i++) {
+
+			if (changeDue >= money[i]) {
+				double newChange = changeDue % money[i];
+				int amount = (int) ((changeDue - newChange) / money[i]);
+				System.out.println(denominations[i] + amount);
+				changeDue = newChange;
+
+			}
+
+		}
+
+	}
+
+}
--- src/com/Skilldistillery/makechange/MakeChangeApp.java
+++ src/com/Skilldistillery/makechange/MakeChangeApp.java
@@ -1,120 +1,55 @@
 package com.Skilldistillery.makechange;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("00.00");
 		
-		System.out.println("How much does the item cost? ");
-		double itemPrice = keyboard.nextDouble();
+		double total = 0;
+		String answer;
+		
+		do {
+			System.out.println("How much does the item cost? ");
+			double itemPrice = keyboard.nextDouble();
+			total = total + itemPrice;
+			System.out.println("Do you want to continue or add another item, type \"Done\" if finished");
+			answer = keyboard.next();
+			
+			
+			
+		} while (!answer.equals("Done"));
+
+		System.out.println("Your total is " + total);
+		System.out.println();
 		
 		System.out.println("How much money did you provide?");
 		double moneyPaid = keyboard.nextDouble();
-		
+
 		double changeDue = 0;
-		
-		
-		
-		if(moneyPaid < itemPrice) {
+
+		if (moneyPaid < total) {
 			System.out.println("That is not enough money!");
+		} else if (moneyPaid == total) {
+			System.out.println("You gave me exact change, thank you!!");
+		} else {
+			System.out.println("Let me get your change.");
+			System.out.println();
+
+			changeDue = moneyPaid - total;
+			System.out.println("I owe you " + df.format(changeDue) );
+			System.out.println();
+
+			ChangeArray changeArray = new ChangeArray();
+			changeArray.cashArry(changeDue);
+
+
+			keyboard.close();
+
 		}
-			else if (moneyPaid == itemPrice) {
-				System.out.println("You gave me exact change, thank you!!");
-		}
-			else {
-				System.out.println("Let me get your change.");
-				System.out.println();
-			
-				changeDue = moneyPaid - itemPrice;
-				System.out.println("I owe you " + changeDue);
-				System.out.println();
-				
-				
-				
-				double newChange = changeDue % 20;
-				int twenties =(int)(changeDue-newChange) / 20;
-				
-				changeDue = newChange;
-				newChange = changeDue % 10;
-				int tens = (int)(changeDue-newChange) / 10;
-				
-				changeDue = newChange;
-				newChange = changeDue % 5;
-				int fives = (int)(changeDue -newChange) / 5 ;
-				
-				changeDue = newChange;
-				newChange = changeDue % 1;
-				int ones = (int)(changeDue - newChange) / 1;
-				
-				changeDue = newChange;
-				newChange = changeDue % .25;
-				int quarters = (int)((changeDue - newChange) / .25);
-				
-				changeDue = newChange;
-				newChange = changeDue % .10;
-				int dimes = (int)((changeDue - newChange) / .10);
-				
-				changeDue = newChange;
-				newChange = changeDue % .05;
-				int nickels = (int)((changeDue - newChange) / .05);
-				
-				changeDue = newChange;
-				newChange= changeDue % .009;
-				int pennies = (int)((changeDue-newChange) / .009);
-			
-			
-				
-				
-				
-				
-				
-				if ( twenties > 0) {
-					System.out.println("twenties: " + twenties);
-				}
-				
-				if ( tens > 0 ) {
-					System.out.println("tens:" + tens);
-				}
-				
-				if ( fives > 0) {
-					System.out.println("fives:" + fives);
-				}
-				
-				if (ones > 0 ) {
-					System.out.println("ones: " + ones);
-				}
-				
-				if (quarters > 0 ) {
-					System.out.println("quarters: " + quarters);
-				}
-				
-				if( dimes > 0 ) {
-					System.out.println("dimes: " + dimes);
-				}
-				
-				if (nickels > 0 ) {
-					System.out.println("nickles: " + nickels);
-				}
-				
-				if ( pennies > 0 ) {
-					System.out.println("pennies: " + pennies);
-				}
-				
-			
-				
-			}
-		
-			
-	
-			
-			
-		keyboard.close();
-		
-		}
-		
-		
+
 	}
-
-
+}
